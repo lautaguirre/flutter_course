c413c48091a353da093430aecd2b2d96345a5dd7
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-11-24 00:24:42 -0300

    added personal_expenses app

---
+++ personal_expenses/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Flutter App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({ this.title });
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Card(
+            color: Colors.blue,
+            elevation: 5,
+            child: Container(
+              width: double.infinity,
+              child: Text('CHART'),
+            ),
+          ),
+          Card(
+            child: Text('Ticio'),
+          ),
+        ],
+      ),
+    );
+  }
+}
