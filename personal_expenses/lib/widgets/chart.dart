f3b89013b00c32929ea1f0b231932f53012b9857
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-12-11 22:56:47 -0300

    updated flutter ; created ChartBar widget ; modified Chart widget

--- personal_expenses/lib/widgets/chart.dart
+++ personal_expenses/lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -20,9 +21,15 @@ class Chart extends StatelessWidget {
       }
 
       return {
-        'day': DateFormat.E().format(weekday),
+        'day': DateFormat.E().format(weekday).substring(0, 1),
         'amount': totalSum,
       };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactions.fold(0.0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
@@ -31,10 +38,21 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: <Widget>[
-
-        ],
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                data['day'],
+                data['amount'],
+                totalSpending == 0 ? 0 : (data['amount'] as double) / totalSpending
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
